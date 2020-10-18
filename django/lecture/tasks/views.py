ae277a5ab043f884384b7327c40562a0b791696a
Dan <me.daniel.meier@gmail.com>
2020-10-18 18:57:53 +0200

    add reverse and empty list

--- django/lecture/tasks/views.py
+++ django/lecture/tasks/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django import forms
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 class NewTaskForm(forms.Form):
     task = forms.CharField(label="New Task")
     # priority = forms.IntegerField(label="Priority", min_value=1, max_value=10)
 
-tasks = ["foo", "bar", "baz"]
+tasks = []
 
 # Create your views here.
 def index(request):
@@ -19,6 +21,7 @@ def add(request):
         if form.is_valid():
             task = form.cleaned_data["task"]
             tasks.append(task)
+            return HttpResponseRedirect(reverse("tasks:index"))
         else:
             return render(request, "tasks/add.html", {
                     "form": form
