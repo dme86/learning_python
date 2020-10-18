9f148c4ef20397031147cd92b150edd1e4b735aa
Dan <me.daniel.meier@gmail.com>
2020-10-18 19:22:07 +0200

    switch to session tasks in append

--- django/lecture/tasks/views.py
+++ django/lecture/tasks/views.py
@@ -15,7 +15,7 @@ def index(request):
         request.session["tasks"] = []
 
     return render(request, "tasks/index.html", {
-            "tasks": request.session["tasks"]
+         "tasks": request.session["tasks"]
         })
 
 def add(request):
@@ -23,7 +23,7 @@ def add(request):
         form = NewTaskForm(request.POST)
         if form.is_valid():
             task = form.cleaned_data["task"]
-            tasks.append(task)
+            request.session["tasks"] += [task]
             return HttpResponseRedirect(reverse("tasks:index"))
         else:
             return render(request, "tasks/add.html", {
