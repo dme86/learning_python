420a7fa71ebf4e9d1867e524a6466b3a19370877
Dan <me.daniel.meier@gmail.com>
2020-10-18 19:08:34 +0200

    switch to session tasks

--- django/lecture/tasks/views.py
+++ django/lecture/tasks/views.py
@@ -7,12 +7,15 @@ class NewTaskForm(forms.Form):
     task = forms.CharField(label="New Task")
     # priority = forms.IntegerField(label="Priority", min_value=1, max_value=10)
 
-tasks = []
+# tasks = []
 
 # Create your views here.
 def index(request):
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
+
     return render(request, "tasks/index.html", {
-            "tasks": tasks
+            "tasks": request.session["tasks"]
         })
 
 def add(request):
