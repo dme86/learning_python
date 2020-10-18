a1c6b1e7e8a2d18d1c86233d89ed4f97340f4081
Dan <me.daniel.meier@gmail.com>
2020-10-18 18:53:47 +0200

    comment priority

--- django/lecture/tasks/views.py
+++ django/lecture/tasks/views.py
@@ -3,7 +3,7 @@ from django import forms
 
 class NewTaskForm(forms.Form):
     task = forms.CharField(label="New Task")
-    priority = forms.IntegerField(label="Priority", min_value=1, max_value=10)
+    # priority = forms.IntegerField(label="Priority", min_value=1, max_value=10)
 
 tasks = ["foo", "bar", "baz"]
 
